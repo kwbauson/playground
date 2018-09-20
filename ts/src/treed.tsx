a36ba5712831e3cb0fbe66738afa483905d19ebd
Keith Bauson <kwbauson@gmail.com>
2018-09-20 16:02:18 -0400

    stuff

--- ts/src/treed.tsx
+++ ts/src/treed.tsx
@@ -1,36 +1,105 @@
 import React from 'react'
+import shortid from 'shortid'
 
 type Tree<T> = {
   label: T
   children: Tree<T>[]
+  key: string
 }
 
-const Char = Symbol('Char')
+interface ViewTypes {}
 
-interface ViewTypes {
-  [Char]: string
-}
-
-type TaggedViewTypes = {
-  [T in keyof ViewTypes]: {
-    type: T
-    value: ViewTypes[T]
-  }
-}
-
-interface Label<T> {
+interface Label<T extends keyof ViewTypes = keyof ViewTypes> {
   type: T
-  value: TaggedViewTypes[T]
+  value: ViewTypes[T]
+  selected: boolean
+  expanded: boolean
 }
 
 type Display = JSX.Element
 
-interface View<T extends keyof ViewTypes> {
-  (label: Label<T>, children: Display[]): Display
+interface ViewProps<T extends keyof ViewTypes> {
+  label: Label<T>
+  children: Display[]
 }
 
-const viewChar: View<'char'> = label => {
-  label.value
+interface View<T extends keyof ViewTypes = keyof ViewTypes> {
+  type: T
+  (props: ViewProps<T>): Display
 }
 
-export const App = () => <></>
+interface ViewTypes {
+  text: string
+  line: string
+  char: string
+  bool: boolean
+}
+
+function mkView<T extends keyof ViewTypes>(
+  type: T,
+  f: (props: ViewProps<T>) => Display,
+): View<T> {
+  const view = f as View<T>
+  view.type = type
+  return view
+}
+
+const textView = mkView('text', ({ children }) => <div>{children}</div>)
+
+const lineView = mkView('line', ({ children }) => <div>{children}</div>)
+
+const charView = mkView('char', ({ label }) => <span>{label.value}</span>)
+
+function textTree(text: string): Tree<Label> {
+  const lines = text.split('\n')
+  return {
+    label: {
+      type: 'text',
+      value: text,
+      selected: true,
+      expanded: true,
+    },
+    children: lines.map(textLineTree),
+    key: shortid(),
+  }
+}
+
+function textLineTree(line: string): Tree<Label> {
+  const chars = line.split('')
+  return {
+    label: {
+      type: 'line',
+      value: line,
+      selected: false,
+      expanded: true,
+    },
+    children: chars.map(textCharTree),
+    key: shortid(),
+  }
+}
+
+function textCharTree(char: string): Tree<Label> {
+  return {
+    label: {
+      type: 'char',
+      value: char,
+      selected: false,
+      expanded: true,
+    },
+    children: [],
+    key: shortid(),
+  }
+}
+
+function viewTree(views: View<any>[], tree: Tree<Label>): JSX.Element {
+  const view = views.find(x => x.type === tree.label.type)!
+  return view({
+    label: tree.label,
+    children: tree.children.map(x => (
+      <React.Fragment key={x.key}>{viewTree(views, x)}</React.Fragment>
+    )),
+  })
+}
+
+export const App = () =>
+  viewTree([textView, lineView, charView], textTree('line 1\nline 2\nline 3'))
