95fa86b5a911700a5ae94b20e17da1c097f609c7
Keith Bauson <kwbauson@gmail.com>
2019-02-02 12:08:03 -0500

    entries and stuff

--- ts/src/App-vtree.tsx
+++ ts/src/App-vtree.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
-import { View, keys } from './vtree'
+import { View, keys, is } from './vtree'
 import * as t from 'io-ts'
 import _ from 'lodash'
+
+class Entry<T, R = any> {
+  constructor(public key: string | number, public value: View<T, R>) {}
+}
+
+const jsonView = View.create()
+  .match(t.union([t.string, t.number, t.boolean, t.null]), ({ value }) => (
+    <code>{JSON.stringify(value)}</code>
+  ))
+  .match(
+    x => Array.isArray(x) || _.isPlainObject(x),
+    ({ children, render }) => {
+      const isArray = Array.isArray(children)
+      const entries = Object.entries(children)
+      const opening = isArray ? '[' : '{'
+      const closing = isArray ? ']' : '}'
+      return (
+        <>
+          <code>{opening}</code>
+          {entries.map(([key, val], i) => (
+            <div key={key} style={{ paddingLeft: 16 }}>
+              {render(new Entry(isArray ? i : key, val))}
+              {i !== entries.length - 1 && <code>,</code>}
+            </div>
+          ))}
+          <code>{closing}</code>
+        </>
+      )
+    },
+  )
+  .match(is(Entry), ({ children: { key, value } }) => (
+    <>
+      <code>{JSON.stringify(key.value)}:&nbsp;</code>
+      {value.render()}
+    </>
+  ))
 
 function isUrl(value: any): value is string {
   return (
@@ -13,33 +49,6 @@ function isUrl(value: any): value is string {
 function isImageUrl(value: any): value is string {
   return isUrl(value) && !!value.match(/\.(jpe?g|gif|png)$/i)
 }
-
-const jsonView = View.create()
-  .match(t.union([t.string, t.number, t.boolean, t.null]), ({ value }) => (
-    <code>{JSON.stringify(value)}</code>
-  ))
-  .match(
-    x => Array.isArray(x) || _.isPlainObject(x),
-    ({ value, children }) => {
-      const isArray = Array.isArray(value)
-      const entries = Object.entries(children)
-      const opening = isArray ? '[' : '{'
-      const closing = isArray ? ']' : '}'
-      return (
-        <>
-          <code>{opening}</code>
-          {entries.map(([key, val], i) => (
-            <div key={key} style={{ paddingLeft: 16 }}>
-              <code>{isArray ? i : JSON.stringify(key)}:&nbsp;</code>
-              {val.render()}
-              {i !== entries.length - 1 && <code>,</code>}
-            </div>
-          ))}
-          <code>{closing}</code>
-        </>
-      )
-    },
-  )
 
 export const App = View.create()
   .match(t.any, <i>UNHANDLED</i>)
@@ -128,6 +137,87 @@ export const App = View.create()
       />
     ),
   )
+  .match<Entry<Todo[]>>(
+    x => is(Entry)(x) && x.key === 'todoMvc',
+    ({ value: entry }) => entry.value,
+  )
+  .match<TodoMVC>(
+    keys('newTodo', 'todos', 'viewing'),
+    ({ children: { newTodo, todos, viewing } }) => (
+      <div>
+        <div>todos</div>
+        <div>
+          <input
+            value={newTodo.value}
+            onChange={e => newTodo.set(e.target.value)}
+            placeholder="What needs to be done?"
+          />
+          <button
+            onClick={() => {
+              todos.set(xs => [
+                ...xs,
+                { text: newTodo.value, completed: false },
+              ])
+            }}
+          >
+            add
+          </button>
+        </div>
+        {todos.children
+          .filter(
+            x =>
+              viewing.value === 'all' ||
+              (viewing.value == 'active' && !x.value.completed) ||
+              (viewing.value === 'completed' && x.value.completed),
+          )
+          .map(x => (
+            <Todo key={x.key} view={x} />
+          ))}
+        <div style={{ justifyContent: 'space-between' }}>
+          <span>{todos.value.filter(x => !x.completed).length} items left</span>
+          <span>
+            <button onClick={() => viewing.set('all')}>all</button>
+            <button onClick={() => viewing.set('active')}>active</button>
+            <button onClick={() => viewing.set('completed')}>completed</button>
+          </span>
+          <button
+            onClick={() => todos.set(todos.value.filter(x => !x.completed))}
+          >
+            clear completed
+          </button>
+        </div>
+      </div>
+    ),
+  )
+  .match({ text: t.string, completed: t.boolean }, ({ children }) => (
+    <div>
+      <input type="check" />
+    </div>
+  ))
   .component(require('./initial-state'))
+
+const Todo: React.FunctionComponent<{ view: View<Todo> }> = ({
+  view: {
+    children: { text, completed },
+  },
+}) => (
+  <div>
+    <button onClick={() => completed.set(x => !x)}>
+      {completed.value ? 'uncomplete' : 'complete'}
+    </button>
+    <span>{text.value}</span>
+  </div>
+)
+
+type TodoMVC = {
+  newTodo: string
+  todos: Todo[]
+  viewing: 'all' | 'active' | 'completed'
+}
+
+type Todo = {
+  text: string
+  completed: boolean
+}
 
 Object.assign(window, { rootView: App.view })
